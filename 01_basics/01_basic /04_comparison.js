c78a4bc4977e209b47bb6209349818b5a9c5e29b
guptaaman9170 <guptaaman9170@gmail.com>
2024-06-22 06:45:52 +0000

    container config done

---
+++ 01_basics/01_basic /04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);//false
+console.log(null == 0);//false
+console.log(null >= 0);//true
+//reason being comparison converts null to zero and == converts null to one !! 
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+// all will come out to be false !!
+// === 
+
+console.log("2" === 2);

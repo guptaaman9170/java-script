10cf91bb1d458f8c2a6442fa447ff4da8e4e65f0
guptaaman9170 <guptaaman9170@gmail.com>
2024-07-14 19:28:08 +0000

    done

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -29,7 +29,7 @@ let val1;
 // val1 = 5 ?? 10                output 5
 // val1 = null ?? 10             output 10
 // val1 = undefined ?? 15        output 15
-val1 = null ?? 10 ?? 20          output 10
+val1 = null ?? 10 ?? 20          //output 10
 
 
 
